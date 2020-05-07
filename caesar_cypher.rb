b60302a3141d3bb7e150835bbaa7babb31eed6b9
Brandon Stottler <bstottler11@gmail.com>
2020-05-06 17:08:21 -0700

    added files

---
+++ caesar_cypher.rb
@@ -0,0 +1,7 @@
+require 'pry'
+
+
+
+#Get the string input from the user and save to a variable. Get the  shift number from the user and save to a variable.
+#Create an array of captital letters and lower case letters. Write a method that for each letter shifts it by the shift number and adds it 
+#to a new string. Once finished, return and print the new string.
